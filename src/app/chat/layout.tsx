06b4710dc4304f52c0eb5fb1ccd2e99bf15fc411
Faiz2979 <faizannabil877@gmail.com>
2025-02-24 16:12:37 +0700

    basic chat

---
+++ src/app/chat/layout.tsx
@@ -0,0 +1,22 @@
+import SidebarWithNavbar from "@/components/SidebarWithNavbar";
+import { Metadata } from "next";
+
+
+export const metadata: Metadata = {
+    title: "TaskCanvas | Chats",
+};
+
+export default function RootLayout({
+    children,
+}: Readonly<{
+    children: React.ReactNode;
+}>) {
+    return (
+        <div
+        className={`antialiased`}
+        >
+            <SidebarWithNavbar/>
+            {children}
+        </div>
+    );
+  }
---
+++ src/pages/chatServices.ts
@@ -0,0 +1,59 @@
+import { db } from "@/lib/firebaseConfig";
+import { addDoc, arrayUnion, collection, doc, getDoc, serverTimestamp, updateDoc } from "firebase/firestore";
+
+export async function sendMessage(chatId: string, senderId: string, text: string) {
+    if (!text.trim()) return;
+
+    const chatRef = doc(db, "chats", chatId);
+    const messagesRef = collection(chatRef, "messages");
+
+    try {
+        await addDoc(messagesRef, {
+            senderId,
+            text,
+            timestamp: serverTimestamp(),
+        });
+
+        await updateDoc(chatRef, {
+            lastMessage: text,
+            updatedAt: serverTimestamp(),
+        });
+
+    } catch (error) {
+        console.error("Error sending message:", error);
+    }
+}
+
+export async function createChat(userId: string, friendId: string) {
+    const chatsRef = collection(db, "chats");
+
+    // Cek apakah chat sudah ada
+    const chatId = [userId, friendId].sort().join("_");
+    const chatRef = doc(db, "chats", chatId);
+    const chatSnap = await getDoc(chatRef);
+
+    if (!chatSnap.exists()) {
+        await updateDoc(chatRef, {
+            members: [userId, friendId],
+            isGroup: false,
+            lastMessage: "",
+            updatedAt: serverTimestamp(),
+        });
+    }
+    return chatId;
+}
+
+export async function createGroupChat(adminId: string, members: string[], groupName: string) {
+    const chatsRef = collection(db, "chats");
+    const newChatRef = doc(chatsRef);
+    
+    await updateDoc(newChatRef, {
+        members: arrayUnion(adminId, ...members),
+        isGroup: true,
+        name: groupName,
+        lastMessage: "",
+        updatedAt: serverTimestamp(),
+    });
+
+    return newChatRef.id;
+}
